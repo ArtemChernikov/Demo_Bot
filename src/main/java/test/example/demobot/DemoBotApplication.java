10ed0bc1d5b529bbe2acf7fecf6f0afed75593ce
OvercomingJunk <overcomingjunk@yandex.ru>
2022-11-18 15:39:01 +0300

    Частично добавил JavaDoc

--- src/main/java/test/example/demobot/DemoBotApplication.java
+++ src/main/java/test/example/demobot/DemoBotApplication.java
@@ -4,13 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * Класс служит точкой запуска Telegram-бота
+ *
  * @author Artem Chernikov
  * @version 1.0
  * @since 17.11.2022
  */
+
 @SpringBootApplication
 public class DemoBotApplication {
-
+    /**
+     * Сама точка запуска
+     *
+     * @param args - входные параметры запуска
+     */
     public static void main(String[] args) {
         SpringApplication.run(DemoBotApplication.class, args);
     }
--- src/main/java/test/example/demobot/service/TelegramBot.java
+++ src/main/java/test/example/demobot/service/TelegramBot.java
@@ -9,6 +9,8 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import test.example.demobot.config.ConfigBot;
 
 /**
+ * Класс описывает модель Telegram-бота
+ *
  * @author Artem Chernikov
  * @version 1.0
  * @since 17.11.2022
@@ -16,22 +18,42 @@ import test.example.demobot.config.ConfigBot;
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
+    /**
+     * Конфигурационный объект {@link ConfigBot}
+     */
     private final ConfigBot config;
 
     public TelegramBot(ConfigBot config) {
         this.config = config;
     }
 
+    /**
+     * Стандартный геттер для имени бота
+     *
+     * @return - возвращает имя бота
+     */
     @Override
     public String getBotUsername() {
         return config.getBotName();
     }
 
+    /**
+     * Стандартный геттер для токена бота
+     *
+     * @return - возвращает токен бота
+     */
     @Override
     public String getBotToken() {
         return config.getBotToken();
     }
 
+    /**
+     * Метод используется для взаимодействия пользователя с ботом
+     * На данный момент присутствует только функция "/start"
+     * Switch добавлен намеренно, для дальнейшего расширения взаимодействий
+     *
+     * @param update - обновление со стороны пользователя чата {@link Update}
+     */
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
@@ -44,11 +66,23 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
+    /**
+     * Метод используется для ответа бота пользователю на команду "/start"
+     *
+     * @param chatId   - id чата пользователя
+     * @param username - имя пользователя
+     */
     private void startCommandExecute(long chatId, String username) {
         String message = "Привет, " + username + "! Как Ваши дела?";
         sendMessage(chatId, message);
     }
 
+    /**
+     * Метод используется для отправки сообщений пользователю по chatId
+     *
+     * @param chatId  - id чата пользователя
+     * @param message - текст сообщения
+     */
     private void sendMessage(long chatId, String message) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(String.valueOf(chatId));
